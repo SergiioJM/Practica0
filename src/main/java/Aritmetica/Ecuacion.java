30a04aae852de495a92e731ab7c9982f0a3907cc
SergiioJM <sergiojimenez10@hotmail.com>
2021-02-23 10:34:23 +0100

    Nuevos test S

--- src/main/java/Aritmetica/Ecuacion.java
+++ src/main/java/Aritmetica/Ecuacion.java
@@ -4,5 +4,5 @@ import java.util.HashSet;
 
 public interface Ecuacion {
      boolean haySolucion();
-    HashSet<Double> solucion();
+     HashSet<Double> solucion();
 }
--- src/test/java/Aritmetica/EcuacionLinealTest.java
+++ src/test/java/Aritmetica/EcuacionLinealTest.java
@@ -14,9 +14,18 @@ class EcuacionLinealTest {
     void haySolucion() {
         Ecuacion objeto = new EcuacionLineal(0,2);
         assertFalse(objeto.haySolucion());
+
         Ecuacion objeto2 = new EcuacionLineal(2,3);
         assertTrue(objeto2.haySolucion());
 
+        Ecuacion objeto3 = new EcuacionLineal(4,6);
+        assertTrue(objeto3.haySolucion());
+
+        Ecuacion objeto4 = new EcuacionLineal(0,0);
+        assertFalse(objeto4.haySolucion());
+
+        Ecuacion objeto5 = new EcuacionLineal(5,0);
+        assertTrue(objeto5.haySolucion());
     }
 
     @Test
@@ -27,9 +36,21 @@ class EcuacionLinealTest {
         solucion.add(-1d);
         assertEquals(solucion, objeto.solucion());
 
-        Ecuacion objeto2 = new EcuacionLineal(2,4);
+        Ecuacion objeto2 = new EcuacionLineal(2,3);
         solucion = new HashSet<Double>();
-        solucion.add(-2d);
+        solucion.add(-1.5);
         assertEquals(solucion, objeto2.solucion());
+
+        Ecuacion objeto3 = new EcuacionLineal(4,6);
+        solucion = new HashSet<Double>();
+        solucion.add(-1.5);
+        assertEquals(solucion,objeto3.solucion());
+
+        Ecuacion objeto4 = new EcuacionLineal(5,0);
+        solucion = new HashSet<Double>();
+        solucion.add(-0d);
+        assertEquals(solucion,objeto4.solucion());
+
+
     }
 }
